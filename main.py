0002fdf545692abbe4fbd245f121151da3926191
ghpascon <gh.pascon@gmail.com>
2025-03-27 15:33:18 -0300

    ajuste para expor porta

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ csrf = CSRFProtect(app)
 
 import_views()
 if __name__=="__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
 
 #pip freeze > requirements.txt
